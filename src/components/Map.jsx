dbdb3eb31a8329caa82823f9ed0d85d22fcec124
Guntrolu Pavan Prasanth <pavanprasanth48850@gmail.com>
2024-05-23 20:39:46 +0530

    Fetching city data in the form

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -14,15 +14,15 @@ import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 import { useGeolocation } from "../hooks/useGeolocation";
 import Button from "./Button";
+import { useUrlPosition } from "../hooks/useUrlPostion";
 
 const Map = () => {
   const [mapPosition, setMapPosition] = useState([40, 0]);
 
+  const [mapLat, mapLng] = useUrlPosition();
+
   const { cities } = useCities();
 
-  const [searchParams] = useSearchParams();
-  const mapLat = searchParams.get("lat");
-  const mapLng = searchParams.get("lng");
   const {
     isLoading: isLoadingPosition,
     position: geolocationPosition,
